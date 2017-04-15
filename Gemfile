2c17e28f34a7c511b2c106e9b6b54595e18f19e8
timostrating <timostrating@home.nl>
2017-04-15 23:39:17 +0200

    started with scss and Deface

--- Gemfile
+++ Gemfile
@@ -6,8 +6,6 @@ git_source(:github) do |repo_name|
 end
 
 
-
-
 gem 'spree', '~> 3.2.0'
 gem 'spree_auth_devise', '~> 3.2.0.beta'
 gem 'spree_gateway', '~> 3.2.0.beta'
---
+++ app/overrides/header.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/shared/_nav_bar",
+                     :name => "site_nav_bar",
+                     :replace => "#logo",
+                     :text => "<h1> WOW </h1>")
